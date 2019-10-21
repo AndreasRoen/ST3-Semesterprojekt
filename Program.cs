b57f7aa27bcdbdc8756db062393e088f172a3b45
KasperDelaxson <43139475+KasperDelaxson@users.noreply.github.com>
2019-10-21 13:03:15 +0200

    Master

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Brewery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
