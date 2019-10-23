b850e3214dadcc779dd360cbc6bef00957da0700
Kenneth Haahr <43138739+vanhaahr@users.noreply.github.com>
2019-10-23 18:22:53 +0200

    added
    added getters and setters
    added constructor with parameters.

---
+++ BusinessLayer/BatchModule/BatchModel.cs
@@ -0,0 +1,22 @@
+﻿namespace BeerProductionSystem.BusinessLayer.BatchModule {
+    class BatchModel {
+
+        private ushort productType;
+        private ushort batchID;
+        private ushort batchSize;
+        private float productionSpeed;
+
+        public ushort ProductType { get => productType; set => productType = value; }
+        public ushort BatchID { get => batchID; set => batchID = value; }
+        public ushort BatchSize { get => batchSize; set => batchSize = value; }
+        public float ProductionSpeed { get => productionSpeed; set => productionSpeed = value; }
+
+        public BatchModel(ushort productType, ushort batchID, ushort batchSize, float productionSpeed) {
+            this.productType = productType;
+            this.batchID = batchID;
+            this.batchSize = batchSize;
+            this.productionSpeed = productionSpeed;
+
+        }
+    }
+}
