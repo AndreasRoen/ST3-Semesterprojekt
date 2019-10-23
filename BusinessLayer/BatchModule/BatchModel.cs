8e2464e42f599d5152c7adc612d1883fe5e80c89
Kenneth Haahr <43138739+vanhaahr@users.noreply.github.com>
2019-10-23 19:12:16 +0200

    rettelser
    getters og setters rettet til ifht gruppens standard.
    Constructor ligeså.

--- BusinessLayer/BatchModule/BatchModel.cs
+++ BusinessLayer/BatchModule/BatchModel.cs
@@ -1,21 +1,16 @@
 ﻿namespace BeerProductionSystem.BusinessLayer.BatchModule {
     class BatchModel {
 
-        private ushort productType;
-        private ushort batchID;
-        private ushort batchSize;
-        private float productionSpeed;
-
-        public ushort ProductType { get => productType; set => productType = value; }
-        public ushort BatchID { get => batchID; set => batchID = value; }
-        public ushort BatchSize { get => batchSize; set => batchSize = value; }
-        public float ProductionSpeed { get => productionSpeed; set => productionSpeed = value; }
+        public ushort ProductType { get; set; }
+        public ushort BatchID { get; set; }
+        public ushort BatchSize { get; set; }
+        public float ProductionSpeed { get; set; }
 
         public BatchModel(ushort productType, ushort batchID, ushort batchSize, float productionSpeed) {
-            this.productType = productType;
-            this.batchID = batchID;
-            this.batchSize = batchSize;
-            this.productionSpeed = productionSpeed;
+            ProductType = productType;
+            BatchID = batchID;
+            BatchSize = batchSize;
+            ProductionSpeed = productionSpeed;
 
         }
     }
