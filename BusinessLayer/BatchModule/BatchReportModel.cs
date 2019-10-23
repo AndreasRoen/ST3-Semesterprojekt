597a58f890200c45cb09ee0d35ccdff193fb8c2a
Kasper <43139475+KasperDelaxson@users.noreply.github.com>
2019-10-23 20:22:46 +0200

    Added code for BatchReportModel & IDataBaseController

---
+++ BusinessLayer/BatchModule/BatchReportModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerProductionSystem.BusinessLayer.BatchModule
+{
+    class BatchReportModel
+    {
+        private ushort batchID { get; set; }
+        private ushort productType { get; set; }
+        private ushort amountOfProductsTotal { get; set; }
+        private ushort amountOfProductsAcceptable { get; set; }
+        private ushort amountOfProductsDefect { get; set; }
+        private Dictionary<String, int> amountOfTimeInStates { get; set; }
+        private List<float> loggingOfTemperature { get; set; }
+        private List<float> loggingOfHumidity { get; set; }
+    }
+}
