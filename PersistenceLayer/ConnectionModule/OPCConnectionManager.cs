c65b20bf9b1c1e9e38834f6217ff51dd9e3c062e
Kenneth Haahr <43138739+vanhaahr@users.noreply.github.com>
2019-10-24 14:48:13 +0200

    connection mannager
    added both Machine URL and SimURL
    connect method
    disconnect method
    Checkconnection method

---
+++ PersistenceLayer/ConnectionModule/OPCConnectionManager.cs
@@ -0,0 +1,28 @@
+﻿using Opc.UaFx.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerProductionSystem.PersistenceLayer.ConnectionModule {
+    class OPCConnectionManager {
+        public OpcClient AccessPoint { get; }
+
+        string machineURL = "opc.tcp://192.168.0.122:4840";
+        string simulationURL = "opc.tcp://127.0.0.1:4840";
+
+        public void CheckConnection() {
+
+        }
+
+        public void ConnectToServer() {
+            AccessPoint.Connect();
+        }
+
+        public void DisconnectFromServer() {
+            AccessPoint.Disconnect();
+        }
+
+    }
+}
