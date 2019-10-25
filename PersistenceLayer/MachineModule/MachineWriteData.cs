bba730970d512edf771644863e5d00409afbb384
Timmy M. C <zlyzart@outlook.com>
2019-10-25 08:57:09 +0200

    updated machineWriteData to use a dictionary

--- PersistenceLayer/MachineModule/MachineWriteData.cs
+++ PersistenceLayer/MachineModule/MachineWriteData.cs
@@ -9,51 +9,54 @@ namespace BeerProductionSystem.PersistenceLayer.MachineModule
 {
     class MachineWriteData : IMachineWriteData
     {
-        private Dictionary<string, string> nodePathDictionary;  //TODO move to Database package
+        private Dictionary<NodeID, string> nodePathDictionary;  //TODO move to Database package
+
+        private enum NodeID { ControlCommand, CommandChangeRequest, MachSpeed, NextBatchID, NextBatchProductType, NextBatchSize };
 
         public MachineWriteData()
         {
-            nodePathDictionary = new Dictionary<string, string>()
+
+            nodePathDictionary = new Dictionary<NodeID, string>()
             {
-                { "ControlCommand", "ns=6;s=::Program:Cube.Command.CntrlCmd" },
-                { "CommandChangeRequest", "ns=6;s=::Program:Cube.Command.CmdChangeRequest" },
-                { "MachSpeed", "ns=6;s=::Program:Cube.Command.MachSpeed" },
-                { "NextBatchID", "ns=6;s=::Program:Cube.Command.Parameter[0].Value" },
-                { "NextBatchProductType", "ns=6;s=::Program:Cube.Command.Parameter[1].Value" },
-                { "NextBatchSize", "ns=6;s=::Program:Cube.Command.Parameter[2].Value" },
+                { NodeID.ControlCommand, "ns=6;s=::Program:Cube.Command.CntrlCmd" },
+                { NodeID.CommandChangeRequest, "ns=6;s=::Program:Cube.Command.CmdChangeRequest" },
+                { NodeID.MachSpeed, "ns=6;s=::Program:Cube.Command.MachSpeed" },
+                { NodeID.NextBatchID, "ns=6;s=::Program:Cube.Command.Parameter[0].Value" },
+                { NodeID.NextBatchProductType, "ns=6;s=::Program:Cube.Command.Parameter[1].Value" },
+                { NodeID.NextBatchSize, "ns=6;s=::Program:Cube.Command.Parameter[2].Value" },
 
             };
         }
 
         public void WriteControlCommand(OpcClient accessPoint, int commandValue)
         {
-            accessPoint.WriteNode(nodePathDictionary["ControlCommand"], commandValue);
+            accessPoint.WriteNode(nodePathDictionary[NodeID.ControlCommand], commandValue);
             WriteCommandChangeRequest(accessPoint);
         }
 
         public void WriteCommandChangeRequest(OpcClient accessPoint)
         {
-            accessPoint.WriteNode(nodePathDictionary["CommandChangeRequest"], true);
+            accessPoint.WriteNode(nodePathDictionary[NodeID.CommandChangeRequest], true);
         }
 
         public void WriteDesiredMachineSpeed(OpcClient accessPoint, float speed)
         {
-            accessPoint.WriteNode(nodePathDictionary["MachSpeed"], speed);
+            accessPoint.WriteNode(nodePathDictionary[NodeID.MachSpeed], speed);
         }
 
         public void WriteNextBatchID(OpcClient accessPoint, float batchID)
         {
-            accessPoint.WriteNode(nodePathDictionary["NextBatchID"], batchID);
+            accessPoint.WriteNode(nodePathDictionary[NodeID.NextBatchID], batchID);
         }
 
         public void WriteNextBatchProductType(OpcClient accessPoint, float productTypeValue)
         {
-            accessPoint.WriteNode(nodePathDictionary["NextBatchProductType"], productTypeValue);
+            accessPoint.WriteNode(nodePathDictionary[NodeID.NextBatchProductType], productTypeValue);
         }
 
         public void WriteNextBatchSize(OpcClient accessPoint, float batchSize)
         {
-            accessPoint.WriteNode(nodePathDictionary["NextBatchSize"], batchSize);
+            accessPoint.WriteNode(nodePathDictionary[NodeID.NextBatchSize], batchSize);
         }
     }
 }
