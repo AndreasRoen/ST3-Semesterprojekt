527115338e4057c73de2bd49a6cc0464af34c114
Kenneth Haahr <43138739+vanhaahr@users.noreply.github.com>
2019-10-25 02:32:10 +0200

    last fix

--- Program.cs
+++ Program.cs
@@ -1,30 +1,16 @@
-﻿using BeerProductionSystem.PersistenceLayer.ConnectionModule;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BeerProductionSystem
-{
-    static class Program
-    {
+namespace BeerProductionSystem {
+    static class Program {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
-        {
-
-            OPCConnectionManager connectionManager = new OPCConnectionManager();
-            connectionManager.CheckConnection();
-            connectionManager.ConnectToServer();
-            connectionManager.CheckConnection();
-            var temperature = connectionManager.AccessPoint.ReadNode("ns=6;s=::Program:Cube.Status.Parameter[3].Value");
-            string hej = temperature.ToString();
-            MessageBox.Show("Temperature: " + hej);
-            connectionManager.DisconnectFromServer();
-
+        static void Main() {
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
