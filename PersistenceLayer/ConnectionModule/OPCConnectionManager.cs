1c21b0036b81d0426eab33094f62f83ce3de50d0
Kenneth Haahr <43138739+vanhaahr@users.noreply.github.com>
2019-10-25 09:03:27 +0200

    added execptions
    exeption if connection not available

--- PersistenceLayer/ConnectionModule/OPCConnectionManager.cs
+++ PersistenceLayer/ConnectionModule/OPCConnectionManager.cs
@@ -10,11 +10,14 @@ namespace BeerProductionSystem.PersistenceLayer.ConnectionModule {
     class OPCConnectionManager {
         public OpcClient AccessPoint { get; set; }
         //string URL = "opc.tcp://192.168.0.122:4840";
-        string URL = "opc.tcp://127.0.0.1:4840";
+        private readonly string URL = "opc.tcp://127.0.0.1:4840";
+        private bool connectionFailed=false;
+        private int count = 0;
+
         public OPCConnectionManager() {
             AccessPoint = new OpcClient(URL);
         }
-
+        // server state was either connected or created, but "created" didn't sound like it wasn't connected
         public void CheckConnection() {
             var state = AccessPoint.State;
             string stat = state.ToString();
@@ -24,11 +27,23 @@ namespace BeerProductionSystem.PersistenceLayer.ConnectionModule {
                 MessageBox.Show("Not Connected");
             }
         }
-
+        //trying to connect up to 3 times.
         public void ConnectToServer() {
-            AccessPoint.Connect();
-        }
-
+            try {
+                AccessPoint.Connect();
+            } catch (Exception ex) {
+                MessageBox.Show("Handled connect exeption. Reason: " + ex.Message);
+                connectionFailed = true;
+            }
+            if (connectionFailed == true && ++count < 3) {
+                MessageBox.Show("Connection Failed, trying again");
+                ConnectToServer();
+                System.Threading.Thread.Sleep(5000);
+            }
+            else {MessageBox.Show("Unable to connect, please try again later");
+                }
+            }
+        //disconnect and clean up
         public void DisconnectFromServer() {
             AccessPoint.Disconnect();
             AccessPoint.Dispose();
