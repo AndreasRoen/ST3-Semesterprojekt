77b23a3128d6c91151764d2bfa8c33400ce80f55
Kenneth Haahr <43138739+vanhaahr@users.noreply.github.com>
2019-10-25 02:28:52 +0200

    CheckConnection
    Finally working
    minor other changes

--- PersistenceLayer/ConnectionModule/OPCConnectionManager.cs
+++ PersistenceLayer/ConnectionModule/OPCConnectionManager.cs
@@ -4,16 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BeerProductionSystem.PersistenceLayer.ConnectionModule {
     class OPCConnectionManager {
-        public OpcClient AccessPoint { get; }
-
-        string machineURL = "opc.tcp://192.168.0.122:4840";
-        string simulationURL = "opc.tcp://127.0.0.1:4840";
+        public OpcClient AccessPoint { get; set; }
+        //string URL = "opc.tcp://192.168.0.122:4840";
+        string URL = "opc.tcp://127.0.0.1:4840";
+        public OPCConnectionManager() {
+            AccessPoint = new OpcClient(URL);
+        }
 
         public void CheckConnection() {
-
+            var state = AccessPoint.State;
+            string stat = state.ToString();
+            if (stat == "Connected") {
+                MessageBox.Show("Connection is Active");
+            } else {
+                MessageBox.Show("Not Connected");
+            }
         }
 
         public void ConnectToServer() {
@@ -22,6 +31,7 @@ namespace BeerProductionSystem.PersistenceLayer.ConnectionModule {
 
         public void DisconnectFromServer() {
             AccessPoint.Disconnect();
+            AccessPoint.Dispose();
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BeerProductionSystem.PersistenceLayer.ConnectionModule;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,17 @@ namespace BeerProductionSystem
         [STAThread]
         static void Main()
         {
+
+            OPCConnectionManager connectionManager = new OPCConnectionManager();
+            connectionManager.CheckConnection();
+            connectionManager.ConnectToServer();
+            connectionManager.CheckConnection();
+            var temperature = connectionManager.AccessPoint.ReadNode("ns=6;s=::Program:Cube.Status.Parameter[3].Value");
+            string hej = temperature.ToString();
+            MessageBox.Show("Temperature: " + hej);
+            connectionManager.DisconnectFromServer();
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
