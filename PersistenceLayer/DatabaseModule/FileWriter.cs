68a97d8bb0abecee0036bb01079d9cf97c75df8f
FrederikH <f.haagensen@hotmail.com>
2019-11-05 11:28:45 +0100

    added a better storing of batchreports in files

--- PersistenceLayer/DatabaseModule/FileWriter.cs
+++ PersistenceLayer/DatabaseModule/FileWriter.cs
@@ -14,11 +14,53 @@ namespace BeerProductionSystem.PersistenceLayer.DatabaseModule
         public bool SaveBatchReport(BatchReportDTO BatchReport)
         {
             string path = Directory.GetCurrentDirectory() + "\\BatchReport_ID:" +  BatchReport.BatchID.ToString() + ".txt" ;
-            string[] report = { BatchReport.BatchID.ToString(), BatchReport.ProductType.ToString(), BatchReport.AmountOfProductsTotal.ToString(),
-            BatchReport.AmountOfProductsAcceptable.ToString(), BatchReport.AmountOfProductsDefect.ToString(), BatchReport.AmountOfTimeInStates.ToString(),
-            BatchReport.LoggingOfTemperature.ToString(), BatchReport.LoggingOfHumidity.ToString()};
+            string batchid = "Batch ID: " + BatchReport.BatchID.ToString();
+            string productType = "Product ID: " + BatchReport.ProductType.ToString();
+            string amountTotal = "Total amount of products: " + BatchReport.AmountOfProductsTotal.ToString();
+            string amountAcc = "Acceptable amount of products: " + BatchReport.AmountOfProductsAcceptable.ToString();
+            string amountDef = "Defect amount of products: " + BatchReport.AmountOfProductsDefect.ToString();
+            string amountState = GetTimeinStates(BatchReport.AmountOfTimeInStates);
+            string logTemp = LoggingOfInfo("Logging of overall temperature:", BatchReport.LoggingOfTemperature);
+            string logHum = LoggingOfInfo("Logging of overall humidity:", BatchReport.LoggingOfHumidity);
+            string[] report = {batchid, productType, amountTotal, amountAcc, amountDef, amountState, logTemp, logHum};
             File.WriteAllLines(path, report);
             return true;
         }
+
+        private string GetTimeinStates(Dictionary<int, int> states)
+        {
+            string stateLines = "";
+            foreach (KeyValuePair<int, int> state in states)
+            {
+                stateLines = states + "Time in state " + state.Key.ToString() + ": " + state.Value.ToString() + ", \t";
+            }
+            return stateLines;
+        }
+
+        private string LoggingOfInfo(string infoAbout, List<float> list)
+        {
+            string logging = infoAbout + "\n";
+            float min = 0;
+            float max = 0;
+            float avg = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if(list[i] > max)
+                {
+                    max = list[i];
+                }
+                if(list[i] < min)
+                {
+                    min = list[i];
+                }
+                avg += list[i];
+            }
+            avg = avg / list.Count;
+
+            logging = logging + "Minimum: " + min.ToString() + "\t Maximum: " + max.ToString() + "\t Average: " + avg.ToString();
+            return logging;
+        }
+
+
     }
 }
